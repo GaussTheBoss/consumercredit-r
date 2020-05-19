f2412ce271e5a471bdcb747be7885f32f2d2a20a
Steve Avsec <stephen.avsec@opendatagroup.com>
2020-05-18 23:48:52 -0500

    Minor fix

--- cc_model.R
+++ cc_model.R
@@ -14,9 +14,9 @@ begin <- function(){
 action <- function(datum){
 	datum$earliest_cr_line <- as.yearmon(datum$earliest_cr_line, 
 					     format="%b-%Y")
-	datum$logit_int_rate <- sapply(datum$int_rate, qlogis)
 	datum$int_rate <- as.numeric(sub("%", "", datum$int_rate, 
 					     fixed=TRUE))/100
+	datum$logit_int_rate <- sapply(datum$int_rate, qlogis)
 	datum$log_loan_amnt <- sapply(datum$loan_amnt, log)
 	datum$log_annual_inc <- sapply(datum$annual_inc, log)
 	datum$home_ownership <- sapply(datum$home_ownership, hos_cleanup)
