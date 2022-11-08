d4e68fd0532f454ca6c5280c6159e3dd0a0d3f7b
merhi-odg <sami.merhi@opendatagroup.com>
2022-11-08 15:14:36 -0500

    fix labels in data

--- cc_model.R
+++ cc_model.R
@@ -2,46 +2,47 @@
 # modelop.slot.1: in-use
 
 library(zoo)
+library(yardstick)
 
 # modelop.init
-begin <- function(){
-	load("model_artifacts.RData")
-	logreg <<- logreg_model
-	predictor <<- predictor
-	hos_cleanup <<- ho_cleanup
-	threshold <<- threshold
+begin <- function() {
+    load("model_artifacts.RData")
+    logreg <<- logreg_model
+    predictor <<- predictor
+    hos_cleanup <<- ho_cleanup
+    threshold <<- threshold
 }
 
-make_prediction <- function(datum){
-	datum$earliest_cr_line <- as.yearmon(datum$earliest_cr_line, 
-					     format="%b-%Y")
-	datum$int_rate <- as.numeric(sub("%", "", datum$int_rate, 
-					     fixed=TRUE))/100
-	datum$logit_int_rate <- sapply(datum$int_rate, qlogis)
-	datum$log_loan_amnt <- sapply(datum$loan_amnt, log)
-	datum$log_annual_inc <- sapply(datum$annual_inc, log)
-	datum$home_ownership <- sapply(datum$home_ownership, hos_cleanup)
-	datum$credit_age = Sys.yearmon() - datum$earliest_cr_line
-	preds <- unname(predict(logreg, datum, type="response"))
-	return(preds)
+make_prediction <- function(datum) {
+    datum$earliest_cr_line <- as.yearmon(datum$earliest_cr_line,
+        format = "%b-%Y"
+    )
+    datum$int_rate <- as.numeric(sub("%", "", datum$int_rate,
+        fixed = TRUE
+    )) / 100
+    datum$logit_int_rate <- sapply(datum$int_rate, qlogis)
+    datum$log_loan_amnt <- sapply(datum$loan_amnt, log)
+    datum$log_annual_inc <- sapply(datum$annual_inc, log)
+    datum$home_ownership <- sapply(datum$home_ownership, hos_cleanup)
+    datum$credit_age <- Sys.yearmon() - datum$earliest_cr_line
+    preds <- unname(predict(logreg, datum, type = "response"))
+    return(preds)
 }
 
 # modelop.score
-action <- function(datum){
-	preds <- make_prediction(datum)
-	outcome <- sapply(preds, predictor)
-	output <- list(outome = outcome, propensity = preds)
-	emit(output)
+action <- function(datum) {
+    preds <- make_prediction(datum)
+    outcome <- sapply(preds, predictor)
+    output <- list(outome = outcome, propensity = preds)
+    emit(output)
 }
 
 # modelop.metrics
-metrics <- function(data){
-	preds <- make_prediction(data)
-	outcomes <- sapply(preds, predictor)
-	cm <- table(factor(outcomes), factor(data$loan_status))
-	conf_mat <- list(
-		c("Charged Off" = cm[1], "Fully Paid" = cm[2]),
-		c("Charged Off" = cm[3], "Fully Paid" = cm[4])
-	)
-	emit(conf_mat)
+metrics <- function(data) {
+    preds <- make_prediction(data)
+    outcomes <- sapply(preds, predictor)
+    data$outcomes <- as.factor(outcomes)
+    data$loan_status <- as.factor(data$loan_status)
+    cm <- yardstick::conf_mat(data = data, truth = loan_status, estimate = outcomes)
+    emit(cm)
 }
